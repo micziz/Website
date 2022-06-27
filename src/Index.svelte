80c08dc7319db55ee8152ecb1b81295362ac25c6
Giancarlo Mazzaro <junk@Macbook-pro-di-junk.local>
2022-06-27 17:07:15 +0200

    INITIAL

---
+++ src/Index.svelte
@@ -0,0 +1,29 @@
+<style>
+  @import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+
+  #title{
+    text-align: center;
+    margin-top: 50px;
+  }
+
+  #bio{
+    text-align: center;
+  }
+
+  #img{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 25px;
+  }
+</style>
+
+<h1 id="title" class="title">micziz</h1>
+
+<p id="bio">Creator of <a href="https://github.com/PassMax/PassMax">@PassMax</a> | I ❤️ svelte and python | Love from Italy 🍕</p>
+
+<div id="img">
+  <img src="images/me.jpg" alt="me" width="250px" height="250px"/>
+</div>
+
+<button>Projects</button><button>Social Media</button>
